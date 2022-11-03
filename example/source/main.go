f5034be8a8d77bb3c32df14e5f8282b3f38e7065
Marlon Gomez <marlonsgy@gmail.com>
2022-11-03 21:29:27 +0000

    measure time for example source

--- example/source/main.go
+++ example/source/main.go
@@ -1,17 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/theobitoproject/kankuro/pkg/source"
 )
 
 func main() {
+	now := time.Now()
+
 	src := newSourceRandomAPI("https://random-data-api.com/api/v2")
 	runner := source.NewSafeSourceRunner(src, os.Stdout, os.Args)
 	err := runner.Start()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Println(time.Since(now))
 }
